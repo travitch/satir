592f6f877f98c49354a6486e35f35126e6f51199
Tristan Ravitch <tristan@nochair.net>
2016-02-16 08:41:24 -0800

    Get rid of TaggedArray

--- src/tagged/mod.rs
+++ src/tagged/mod.rs
@@ -2,18 +2,13 @@ use std::marker::PhantomData;
 
 // Type-safe array indexing
 
-pub struct TaggedArray<I,T> {
-    index_type: PhantomData<I>,
-    tagged_array: [T],
-}
-
 pub trait TaggedIndexable {
     fn tag_index(&self) -> usize;
 }
 
-pub fn tagged_index<I, T>(arr : &TaggedArray<I, T>, ix : I) -> T
+pub fn tagged_index<I, T>(arr : &TaggedVec<I, T>, ix : I) -> T
     where I : TaggedIndexable, T : Copy {
-    arr.tagged_array[ix.tag_index()]
+    arr.tagged_vec[ix.tag_index()]
 }
 
 pub struct TaggedVec<I,T> {
