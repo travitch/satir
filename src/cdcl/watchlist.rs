8b332451ece02e378186d0bf85f656795d53803a
Tristan Ravitch <tristan@nochair.net>
2016-02-17 10:45:21 -0800

    Implement unwatch_literal

--- src/cdcl/watchlist.rs
+++ src/cdcl/watchlist.rs
@@ -5,7 +5,22 @@ use cdcl::env;
 use tagged;
 
 pub fn unwatch_literal(env : &mut env::SolverEnv, con : &constraint::Constraint, lit : core::Literal) -> () {
-    
+    let ref mut watchers = &mut env.watchlist[lit];
+    let mut rem_ix = 0;
+    let mut found_con = false;
+    for (ix, constraint) in watchers.iter().enumerate() {
+        if constraint.unique_id() == con.unique_id() {
+            rem_ix = ix;
+            found_con = true;
+            break;
+        }
+    }
+
+    if !found_con {
+        panic!("No constraint found in unwatch_literal");
+    }
+
+    watchers.swap_remove(rem_ix);
 }
 
 pub fn watch_literal<'a>(env : &mut env::SolverEnv<'a>, con : &'a constraint::Constraint, lit : core::Literal) -> () {
